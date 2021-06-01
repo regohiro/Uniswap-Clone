dd4c8fe09fe7ed56bc4eb588b9cd5c784d25d245
REGO350 <riokanehiro228@gmail.com>
2021-06-01 21:45:09 +0900

    uninstall unused package + ERC20.sol fix

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -4,32 +4,56 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract ERC20 is IERC20, IERC20Metadata{
-  string override public name;
-  string override public symbol;
-  uint8 override public decimals;
-  uint256 override public totalSupply;
+  string private name_;
+  string private symbol_;
+  uint8 private decimals_;
+  uint256 private totalSupply_;
 
-  mapping(address => uint256) override public balanceOf;
-  mapping(address => mapping(address => uint256)) override public allowance;
+  mapping(address => uint256) private balance;
+  mapping(address => mapping(address => uint256)) private allowances;
 
   constructor(string memory _name, string memory _symbol, uint _decimals, uint _totalSupply){
-    name = _name;
-    symbol = _symbol;
-    decimals = uint8(_decimals);
-    totalSupply = _totalSupply;
-    balanceOf[msg.sender] = totalSupply;
+    name_ = _name;
+    symbol_ = _symbol;
+    decimals_ = uint8(_decimals);
+    totalSupply_ = _totalSupply;
+    balance[msg.sender] = totalSupply_;
   }
 
+  function name() external view override returns(string memory){
+    return name_;
+  }
+
+  function symbol() external view override returns(string memory){
+    return symbol_;
+  }
+
+  function decimals() external view override returns(uint8){
+    return decimals_;
+  }
+
+  function totalSupply() external view override returns(uint256){
+    return totalSupply_;
+  }
+
+  function balanceOf(address _account) external view override returns(uint256){
+    return balance[_account];
+  }
+
+  function allowance(address _owner, address _spender) external view override returns(uint256){
+    return allowances[_owner][_spender];
+  }  
+
   function approve(address _spender, uint256 _amount) override external returns (bool) {
-    allowance[msg.sender][_spender] = _amount;
+    allowances[msg.sender][_spender] = _amount;
     emit Approval(msg.sender, _spender, _amount);
     return true;
   }
 
   function transferFrom(address _from, address _to, uint256 _amount) override external returns (bool){
-    require(allowance[_from][msg.sender] >= _amount, "ERC20: transfer amount exceeds allowance");
+    require(allowances[_from][msg.sender] >= _amount, "ERC20: transfer amount exceeds allowances");
     _transfer(_from, _to, _amount);
-    allowance[_from][msg.sender] -= _amount;
+    allowances[_from][msg.sender] -= _amount;
     return true;
   }
 
@@ -42,9 +66,9 @@ contract ERC20 is IERC20, IERC20Metadata{
     require(_from != address(0), "ERC20: transfer from the zero address");
     require(_to != address(0), "ERC20: transfer to the zero address");
     require(_to != _from, "ERC20: recipient and sender is the same");
-    require(balanceOf[_from] >= _amount, "ERC20: transfer amount exceeds balance");
-    balanceOf[_from] -= _amount;
-    balanceOf[_to] += _amount;
+    require(balance[_from] >= _amount, "ERC20: transfer amount exceeds balance");
+    balance[_from] -= _amount;
+    balance[_to] += _amount;
     emit Transfer(_from, _to, _amount);
   }
 }
